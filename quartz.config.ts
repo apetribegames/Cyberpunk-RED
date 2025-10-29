944048a5d9ea4b9e4cb8fefc92e04c700855f015
apetribegames <apetribegames@gmail.com>
2025-10-29 15:27:15 -0400

    Quartz sync: Oct 29, 2025, 3:27 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -29,12 +29,12 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#1C1B19",
+          light: "#faf8f8",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
-          darkgray: "#BAA67F",
-          dark: "#BAA67F",
-          secondary: "#EF2F27",
+          darkgray: "#4e4e4e",
+          dark: "#2b2b2b",
+          secondary: "#284b63",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#fff23688",
